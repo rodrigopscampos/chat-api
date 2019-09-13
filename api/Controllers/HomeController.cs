f24a3461769cd04f69098ada97f1ed1fe0b666fd
Rodrigo Campos <rodrigo.campos@intelitrader.com.br>
2019-09-13 11:09:52 -0300

    adiciona redirectionamento da raiz para a index.html

---
+++ api/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace chat_api.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            return Redirect("/index.html");
+        }
+    }
+}
